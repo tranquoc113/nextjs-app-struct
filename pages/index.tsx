851c6edf8d07c256909bb2739f82a3ff34437e35
tranquoc631 <tranquoc631@gmail.com>
2021-04-20 13:33:33 +0700

    add ts

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+function HomePage() {
+  // let finname:string ='abc';
+  return <div>
+    <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    
+    Welcome to Next.js !</div>
+}
+
+export default HomePage
---
+++ pages/user/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+export default function User() {
+    return <>
+
+        <Head>
+            <title>User</title>
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <div>User</div>
+    </>
+}
